22530b0d2d7ad96677cd0e91af7b7b5b801aa5b9
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 15:04:25 +0300

    create model pair

---
+++ core/models/pair.py
@@ -0,0 +1,10 @@
+from sqlalchemy import String, BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
+from .base import Base
+
+
+class Pair(Base):
+    user_owner_telegram_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    user_pair_telegram_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    invite_code: Mapped[str] = mapped_column(String, nullable=True)
+    status: Mapped[str] = mapped_column(String)

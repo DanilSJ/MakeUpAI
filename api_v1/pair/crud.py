211a52c1e9f05d71fda1f587f78145893b8ddecd
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 15:17:54 +0300

    add func join_pair

--- api_v1/pair/crud.py
+++ api_v1/pair/crud.py
@@ -4,11 +4,30 @@ from sqlalchemy.exc import StatementError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, BigInteger
 from core.models import Pair
-from . import schemas
+from .schemas import PairSchema, RegisterSchema, InviteSchema
 
 
-async def create_pair(session: AsyncSession, user_in: schemas.RegisterSchema) -> Pair:
+async def create_pair(session: AsyncSession, user_in: RegisterSchema) -> Pair:
     pair = Pair(**user_in.model_dump())
     session.add(pair)
     await session.commit()
     return pair
+
+async def join_pair(session: AsyncSession, user_in: InviteSchema) -> PairSchema:
+    stmt = select(Pair).where(Pair.invite_code == user_in.invite_code)
+    result = await session.execute(stmt)
+    user = result.scalars().first()
+
+    if user:
+        try:
+            for field, value in user_in.model_dump(exclude_unset=True).items():
+                setattr(user, field, value)
+
+            await session.commit()
+            await session.refresh(user)
+
+            return PairSchema.model_validate(user)
+        except StatementError:
+            raise HTTPException(status_code=400, detail="Incorrect arguments")
+
+    raise HTTPException(status_code=404, detail="Pier does not exist")

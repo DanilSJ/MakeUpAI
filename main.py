cdf8d7c9192f3c57979232876dbd81f976d412c7
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 11:59:53 +0300

    set name on users

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(
     version="0.1.0",
 )
 
-app.include_router(router=user_router, tags=["user"], prefix="/user")
+app.include_router(router=user_router, tags=["users"], prefix="/users")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=7777)

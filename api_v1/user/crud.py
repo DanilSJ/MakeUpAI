6c15f2f8e90687a46da7ccd997e1c4a0c2709ff3
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 12:11:54 +0300

    add func update_status

--- api_v1/user/crud.py
+++ api_v1/user/crud.py
@@ -1,7 +1,8 @@
 from fastapi import HTTPException
 from sqlalchemy.engine import Result
+from sqlalchemy.exc import StatementError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, BigInteger
 from core.models import User
 from . import schemas
 
@@ -22,3 +23,23 @@ async def create_user(session: AsyncSession, user_in: schemas.RegisterSchema) ->
 async def get_user(session: AsyncSession, user_id) -> User | None:
     return await session.get(User, user_id)
 
+async def update_status(
+    session: AsyncSession, telegram_id: BigInteger, user_in: schemas.UpdateUserSchema
+) -> schemas.UserSchema:
+    stmt = select(User).where(User.telegram_id == telegram_id)
+    result = await session.execute(stmt)
+    user = result.scalars().first()
+
+    if user:
+        try:
+            for field, value in user_in.model_dump(exclude_unset=True).items():
+                setattr(user, field, value)
+
+            await session.commit()
+            await session.refresh(user)
+
+            return schemas.UserSchema.model_validate(user)
+        except StatementError:
+            raise HTTPException(status_code=400, detail="Incorrect arguments")
+
+    raise HTTPException(status_code=404, detail="User does not exist")

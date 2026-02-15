5eff21584ec850b2c067fff81e2fac7ad12e5c65
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 15:24:35 +0300

    add generate invite_code

--- api_v1/pair/crud.py
+++ api_v1/pair/crud.py
@@ -1,16 +1,45 @@
+import secrets
+import string
+
 from fastapi import HTTPException
 from sqlalchemy.engine import Result
 from sqlalchemy.exc import StatementError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, BigInteger
+from sqlalchemy import select, BigInteger, exists
 from core.models import Pair
 from .schemas import PairSchema, RegisterSchema, InviteSchema
 
+ALPHABET = string.ascii_letters + string.digits
+CODE_LENGTH = 8
+
+
+async def generate_unique_invite_code(session: AsyncSession) -> str:
+    while True:
+        invite_code = ''.join(secrets.choice(ALPHABET) for _ in range(CODE_LENGTH))
+
+        query = select(exists().where(Pair.invite_code == invite_code))
+        result = await session.execute(query)
+        is_exists = result.scalar()
+
+        if not is_exists:
+            return invite_code
+
 
 async def create_pair(session: AsyncSession, user_in: RegisterSchema) -> Pair:
+    invite_code = await generate_unique_invite_code(session)
+
+    user_in.invite_code = invite_code
+
     pair = Pair(**user_in.model_dump())
     session.add(pair)
-    await session.commit()
+
+    try:
+        await session.commit()
+        await session.refresh(pair)
+    except Exception as e:
+        await session.rollback()
+        raise HTTPException(status_code=400, detail=f"Error creating pair: {str(e)}")
+
     return pair
 
 async def join_pair(session: AsyncSession, user_in: InviteSchema) -> PairSchema:

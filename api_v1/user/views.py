044076365d1abb67df261827f04b59943900c720
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 12:04:57 +0300

    add endpoint get_user

--- api_v1/user/views.py
+++ api_v1/user/views.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy import BigInteger
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper
 from . import schemas
@@ -13,3 +14,10 @@ async def create_user(
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.create_user(session=session, user_in=user_in)
+
+@router.get("/{user_id}/", response_model=schemas.UserSchema)
+async def get_user(
+    user_id: BigInteger,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.get_user(session=session, user_id=user_id)

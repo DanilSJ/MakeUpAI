f63c8862aa2895ef2fb6bc2ae64cfe53aa208bce
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 15:04:48 +0300

    create endpoint create_pair

---
+++ api_v1/pair/views.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import BigInteger
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.models import db_helper
+from . import schemas
+from . import crud
+
+router = APIRouter()
+
+
+@router.post("/", response_model=schemas.PairSchema)
+async def create_pair(
+    user_in: schemas.RegisterSchema,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.create_pair(session=session, user_in=user_in)

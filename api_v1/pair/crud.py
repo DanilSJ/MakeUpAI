5fb7d3c085d8c4c78e938baea4062e9b9f295d9d
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 19:02:09 +0300

    add func get_pair, update_status_pair

--- api_v1/pair/crud.py
+++ api_v1/pair/crud.py
@@ -2,12 +2,11 @@ import secrets
 import string
 
 from fastapi import HTTPException
-from sqlalchemy.engine import Result
 from sqlalchemy.exc import StatementError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, BigInteger, exists
+from sqlalchemy import select, exists
 from core.models import Pair
-from .schemas import PairSchema, RegisterSchema, InviteSchema
+from .schemas import PairSchema, RegisterSchema, InviteSchema, UpdateStatusSchema
 
 ALPHABET = string.ascii_letters + string.digits
 CODE_LENGTH = 8
@@ -23,7 +22,6 @@ async def generate_unique_invite_code(session: AsyncSession) -> str:
 
         if not is_exists:
             return invite_code
-
 
 async def create_pair(session: AsyncSession, user_in: RegisterSchema) -> Pair:
     invite_code = await generate_unique_invite_code(session)
@@ -56,6 +54,28 @@ async def join_pair(session: AsyncSession, user_in: InviteSchema) -> PairSchema:
             await session.refresh(user)
 
             return PairSchema.model_validate(user)
+        except StatementError:
+            raise HTTPException(status_code=400, detail="Incorrect arguments")
+
+    raise HTTPException(status_code=404, detail="Pier does not exist")
+
+async def get_pair(session: AsyncSession, pair_id: int) -> Pair | None:
+    return await session.get(Pair, pair_id)
+
+async def update_status_pair(session: AsyncSession, pair_id: int, pair_in: UpdateStatusSchema) -> PairSchema:
+    stmt = select(Pair).where(Pair.pair_id == pair_id)
+    result = await session.execute(stmt)
+    data = result.scalars().first()
+
+    if data:
+        try:
+            for field, value in pair_in.model_dump(exclude_unset=True).items():
+                setattr(data, field, value)
+
+            await session.commit()
+            await session.refresh(data)
+
+            return PairSchema.model_validate(data)
         except StatementError:
             raise HTTPException(status_code=400, detail="Incorrect arguments")
 

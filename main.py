b14c80377c9d75c410e45f8ab587d4b7eaaf9c96
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 22:06:03 +0300

    add pair_router, test_router

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 from api_v1.user.views import router as user_router
+from api_v1.pair.views import router as pair_router
+from api_v1.test.views import router as test_router
 
 
 @asynccontextmanager
@@ -17,6 +19,8 @@ app = FastAPI(
 )
 
 app.include_router(router=user_router, tags=["users"], prefix="/users")
+app.include_router(router=pair_router, tags=["pairs"], prefix="/pairs")
+app.include_router(router=test_router, tags=["test"], prefix="/test")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=7777)

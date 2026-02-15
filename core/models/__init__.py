b73e4d63fb37f9cae7c241046b2920bb71aee66b
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 15:05:11 +0300

    add model pair

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,10 +1,12 @@
 __all__ = [
     "Base",
     "User",
+    "Pair",
     "DatabaseHelper",
     "db_helper",
 ]
 
 from .user import User
+from .pair import Pair
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper

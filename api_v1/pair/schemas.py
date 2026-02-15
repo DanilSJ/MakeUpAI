eb6faf8935e01a7cd2957cd49c66e94bb328c8d9
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 22:06:29 +0300

    fix bug

--- api_v1/pair/schemas.py
+++ api_v1/pair/schemas.py
@@ -4,18 +4,19 @@ from pydantic import BaseModel, ConfigDict
 class PairSchema(BaseModel):
     id: int
     user_owner_telegram_id: int
-    user_pair_telegram_id: int | None
-    invite_code: str | None
+    user_pair_telegram_id: int | None = None
+    invite_code: str
     status: str
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class RegisterSchema(BaseModel):
-    telegram_id: int
+    user_owner_telegram_id: int
     status: str
 
     model_config = ConfigDict(from_attributes=True)
+
 
 class InviteSchema(BaseModel):
     user_pair_telegram_id: int
@@ -23,8 +24,8 @@ class InviteSchema(BaseModel):
 
     model_config = ConfigDict(from_attributes=True)
 
+
 class UpdateStatusSchema(BaseModel):
     status: str
 
     model_config = ConfigDict(from_attributes=True)
-

cc6bebdb4d528f1e8ac3051aad16132cc9a8735d
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 22:06:12 +0300

    fix bug field

--- core/models/pair.py
+++ core/models/pair.py
@@ -1,10 +1,10 @@
-from sqlalchemy import String, BigInteger
+from sqlalchemy import String, BigInteger, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
 
 class Pair(Base):
-    user_owner_telegram_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    user_pair_telegram_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    invite_code: Mapped[str] = mapped_column(String, nullable=True, unique=True)
+    user_owner_telegram_id: Mapped[int] = mapped_column(BigInteger)
+    user_pair_telegram_id: Mapped[int] = mapped_column(BigInteger, nullable=True)
+    invite_code: Mapped[str] = mapped_column(String, unique=True)
     status: Mapped[str] = mapped_column(String)

df970c6afe3870f6252f6ce0a75bb92591f2d8ab
DanilSJ <danilchagarnoy@mail.ru>
2026-02-15 15:18:07 +0300

    add endpoint join_pair

--- api_v1/pair/views.py
+++ api_v1/pair/views.py
@@ -8,9 +8,16 @@ from . import crud
 router = APIRouter()
 
 
-@router.post("/", response_model=schemas.PairSchema)
+@router.post("/create/", response_model=schemas.PairSchema)
 async def create_pair(
     user_in: schemas.RegisterSchema,
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.create_pair(session=session, user_in=user_in)
+
+@router.post("/join/", response_model=schemas.PairSchema)
+async def join_pair(
+    user_in: schemas.InviteSchema,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.join_pair(session=session, user_in=user_in)
